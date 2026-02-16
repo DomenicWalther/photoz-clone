fe494057f9dd110bc0520b07132163425f55b1bd
DomenicWalther <domenicwalthercreatiiv@gmail.com>
2026-02-16 15:21:20 +0100

    feat: delete photo by ID

--- src/main/java/de/domenicwalther/photozclone/PhotosController.java
+++ src/main/java/de/domenicwalther/photozclone/PhotosController.java
@@ -1,6 +1,7 @@
 package de.domenicwalther.photozclone;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +38,16 @@ public class PhotosController {
         }
         return photo;
     }
+
+
+    @DeleteMapping("photos/{id}")
+    public Photo delete(@PathVariable String id){
+        Photo photo = db.remove(id);
+        if(photo == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return photo;
+    }
+
 
 }
